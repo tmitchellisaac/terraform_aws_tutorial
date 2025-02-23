20bf879faf199f6b4f74d52a1e1c398f83e48dcd
Isaac Mitchell <tmitchellisaac@gmail.com>
2025-02-23 12:23:19 -0500

    Update cloud workspace credentials

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+    cloud {
+    organization = "Brumley_gap"
+    workspaces {
+      name = "learn-terraform-aws"
+    }
+  }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
